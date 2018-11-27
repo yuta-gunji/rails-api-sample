671b56874cfc00df62a5accc8dc35e98aa61f700
Yuta Gunji <yuta.g05@gmail.com>
2018-11-27 15:40:47 +0900

    Add authentication with github

---
+++ spec/lib/user_authenticator_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe UserAuthenticator do
+  describe '#perform' do
+    let(:authenticator) { described_class.new('sample_code') }
+    subject { authenticator.perform }
+
+    context 'when code is incorrect' do
+      let(:error) {
+        double('Sawyre::Resource', error: "bad_velification_code")
+      }
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return(error)
+      end
+
+      it 'should raise an error' do
+        expect{ subject }.to raise_error(
+          UserAuthenticator::AuthenticationError
+        )
+        expect(authenticator.user).to be_nil
+      end
+    end
+
+    context 'when code is correct' do
+      let(:user_data) do
+        {
+          login: 'jsmith1',
+          url: 'http://example.com',
+          avatar_url: 'http://example.com/avatar',
+          name: 'John Smith'
+        }
+      end
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return('validaccesstoken')
+        allow_any_instance_of(Octokit::Client).to receive(
+          :user).and_return(user_data)
+      end
+
+      it 'should save user when does not exist' do
+        expect{ subject }.to change{ User.count }.by(1)
+      end
+    end
+  end
+end
