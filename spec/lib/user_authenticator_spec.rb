36983f0ddaa891ae3e71f685a62af49531bdf409
Yuta Gunji <yuta.g05@gmail.com>
2018-11-27 16:02:43 +0900

    Small refactoring

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -43,6 +43,12 @@ describe UserAuthenticator do
       it 'should save user when does not exist' do
         expect{ subject }.to change{ User.count }.by(1)
       end
+
+      it 'should reuse already registered user' do
+        user = create :user, user_data
+        expect{ subject }.not_to change{ User.count }
+        expect(authenticator.user).to eq(user)
+      end
     end
   end
 end
